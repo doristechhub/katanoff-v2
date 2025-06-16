d3ef82715406fa7cb2b204f23b713701ac3eb786
yatindoris <yatindoris@gmail.com>
2025-06-16 14:47:21 +0530

    update userpanel

--- userpanel/src/app/auth/login/page.jsx
+++ userpanel/src/app/auth/login/page.jsx
@@ -8,18 +8,17 @@ const Login = () => {
     <div className="flex lg:flex-row h-screen">
       {/* Left Side */}
       <div className="lg:block hidden w-full lg:w-3/5 h-screen">
-        <CustomImg
-          srcAttr={handWithRing}
-          className="w-full h-full"
-          priority
-        />
+        <CustomImg srcAttr={handWithRing} className="w-full h-full" priority />
       </div>
 
       {/* Right Side */}
       <div className="w-full h-full lg:w-1/2 flex items-center lg:justify-between px-4 py-12 md:px-32 md:py-52 lg:p-[75px_100px_75px_22px]">
         <div className="flex flex-col justify-center gap-10 md:gap-24 2xl:gap-32 lg:justify-between w-full lg:h-full">
           <Link href={"/"} className="flex justify-center">
-            <CustomImg srcAttr={textLogo} className="w-40 md:w-60 lg:w-70 2xl:w-80 " />
+            <CustomImg
+              srcAttr={textLogo}
+              className="w-40 md:w-60 lg:w-70 2xl:w-80 "
+            />
           </Link>
           <LoginForm />
         </div>
