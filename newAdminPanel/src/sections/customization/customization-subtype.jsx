9ecc39e2e64a7b87be2fb526524ea27eb2d7dc3b
yatindoris <yatindoris@gmail.com>
2025-07-10 17:54:52 +0530

    update admin panel

--- newAdminPanel/src/sections/customization/customization-subtype.jsx
+++ newAdminPanel/src/sections/customization/customization-subtype.jsx
@@ -373,8 +373,6 @@ const CustomizationSubType = () => {
                             {x?.image ? (
                               <Box
                                 sx={{
-                                  width: '100%',
-                                  height: '100%',
                                   borderRadius: 1,
                                   display: 'flex',
                                   justifyContent: 'center',
--- newAdminPanel/src/sections/diamondShape/diamond-shape.jsx
+++ newAdminPanel/src/sections/diamondShape/diamond-shape.jsx
@@ -290,8 +290,6 @@ const DiamondShape = () => {
                               {x?.image ? (
                                 <Box
                                   sx={{
-                                    width: '100%',
-                                    height: '100%',
                                     borderRadius: 1,
                                     display: 'flex',
                                     justifyContent: 'center',
