dc862cc2aee32054bf1e3f0be53cadfbae19f86a
yatindoris <yatindoris@gmail.com>
2025-08-18 12:44:03 +0530

    update code for google analytics id

--- userpanel/src/app/layout.js
+++ userpanel/src/app/layout.js
@@ -2,16 +2,25 @@ import StoreProvider from "@/store/provider";
 import "./globals.css";
 import { Layout } from "@/components/dynamiComponents";
 import { generateMetadata } from "@/_utils/metaConfig";
+import { googleAnalyticsId, googleSiteVerificationId } from "@/_helper";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
-export const metadata = generateMetadata()
+export const metadata = generateMetadata();
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content={googleSiteVerificationId}
+        />
+      </head>
       <body>
         <StoreProvider>
           <Layout>{children}</Layout>
         </StoreProvider>
       </body>
+      <GoogleAnalytics gaId={googleAnalyticsId} />
     </html>
   );
 }
